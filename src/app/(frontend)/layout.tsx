40e1b031f9ad73a0917f9f4aa0532ca34e931fa2
Drew Watkins <dwatkins@exitwithella.io>
2026-01-20 16:23:00 -0500

    feat: add Oatmeal components

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -11,6 +11,18 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Familjen+Grotesk:ital,wght@0,400..700;1,400..700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <main>{children}</main>
       </body>
